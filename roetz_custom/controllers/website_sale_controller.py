25d2b27fc0f12f00bcb957551f918bc04ee79227
Stefan Rijnhart <stefan@opener.am>
2016-04-14 20:29:37 +0200

    [ADD] Website partners by city [FIX] Flake8

--- roetz_custom/controllers/website_sale_controller.py
+++ roetz_custom/controllers/website_sale_controller.py
@@ -1,20 +1,24 @@
-from opener.addons.website_sale.controllers.main import website_sale
+from openerp import http
+from openerp.addons.website_sale.controllers.main import website_sale
 
 
 class SaleController(website_sale):
 
     def checkout_form_save(self, checkout):
         """  Inject context key honoured in res.partner::write() """
-        self.request.context['mail_off'] == True
+        http.request.context['mail_off'] = True
         res = super(SaleController, self).checkout_form_sale(checkout)
-        del self.request.context['mail_off']
+        del http.request.context['mail_off']
         return res
 
-    @http.route('/shop/payment/validate', type='http', auth="public", website=True)
-    def payment_validate(self, transaction_id=None, sale_order_id=None, **post):
-        """  Inject context key honoured in sale.order::action_quotation_send() """
-        self.request.context['mail_off'] == True
-        res = super_SaleController, self).payment_validate(
+    @http.route('/shop/payment/validate', type='http', auth="public",
+                website=True)
+    def payment_validate(
+            self, transaction_id=None, sale_order_id=None, **post):
+        """  Inject context key honoured in sale.order::action_quotation_send()
+        """
+        http.request.context['mail_off'] = True
+        res = super(SaleController, self).payment_validate(
             transaction_id=transaction_id, sale_order_id=sale_order_id, **post)
-        del self.request.context['mail_off']
+        del http.request.context['mail_off']
         return res
