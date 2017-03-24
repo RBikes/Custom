7546e0e6bfbdaba55a5239360b4d22a1d820edeb
Stefan Rijnhart <stefan@opener.am>
2017-03-24 19:27:17 +0100

    [UPD] 8.0.1.1.0

--- roetz_custom/__openerp__.py
+++ roetz_custom/__openerp__.py
@@ -20,7 +20,7 @@
 {
     "name": "Various customizations",
     "category": "Custom",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "author": "Opener B.V.",
     "website": 'https://opener.am',
     "depends": [
